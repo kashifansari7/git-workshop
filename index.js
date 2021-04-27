3890089c2061f0779a9688d3145a2f6e547208bc
Kashif <kashif@leewayhertz.com>
2021-04-27 17:34:56 +0530

    another change is again made in the index.js file

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 // var b= 20;
 // console.log(a+b);
 
-var a = 30;
-var b = 40;
-console.log(a*b);
+// var a = 30;
+// var b = 40;
+// console.log(a*b);
+ var a =90;
+ var b =10;
+ console.log(a/b);
