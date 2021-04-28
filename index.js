70bc257248f28f632fb35176864ff8a36489f6f1
Kashif <kashif@leewayhertz.com>
2021-04-28 12:00:50 +0530

    new change is made

--- index.js
+++ index.js
@@ -5,8 +5,11 @@
 // var a = 30;
 // var b = 40;
 // console.log(a*b);
- var a =90;
- var b =10;
- var c = 30;
- console.log(a-b);
- console.log(a+b-c);
+//  var a =90;
+//  var b =10;
+//  var c = 30;
+//  console.log(a-b);
+//  console.log(a+b-c);
+var a = 10;
+var b = 20;
+console.log(a+b);
